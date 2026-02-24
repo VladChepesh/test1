04962747a199db2f75b54f5badb33cb14bbaeefb
VladChepesh <nourovv@gmail.com>
2026-02-24 11:38:54 +0100

    app.js

--- app.js
+++ app.js
@@ -34,11 +34,12 @@ function filterSolutions(){
     const filtered = solutionsData.filter(item=>
     const matchesStatus = selectedStatus === "all" || item.status.toLowerCase() === selectedStatus.toLowerCase();
     const matchesSearch = item.name.toLowerCase().includes(searchText) || item.descriptionShort.toLowerCase().includes(searchText);
- }); 
+ });)
  renderSolutions(filtered);
 }
-searchInput.addEventListener("input", filterSolutions);
 statusSelect.addEventListener("change", filterSolutions);
+searchInput.addEventListener("input", filterSolutions);
+
 
 
 
