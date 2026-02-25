8562bfd31bc0e78b75ec3eafecf168f101ec39c2
VladChepesh <nourovv@gmail.com>
2026-02-25 10:48:31 +0100

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 console.log ("JS loaded")
 const container = document.getElementById ("solutionsContainer");
 const statusSelect = document.getElementById("statusSelect");
-const searchInput = document.getElementById("searchInput")
+const searchInput = document.getElementById("searchInput");
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modalTitle");
+const modalDescription = document.getElementById("modalDescription");
+const modalStatus = document.getElementById("modalStatus");
+const closeBtn = document.getElementById("close-btn");
 let solutionsData = [];
 fetch("data/solutions.json")
 .then(response=> response.json())
@@ -21,7 +26,7 @@ function renderSolutions(data){
         <p><strong>Kontakt:</strong>${item.owner}</p>
         <p><a href="${item.link}" target="_blank">Mehr erfahren</a></p>
         `;
-        
+        card.addEventListener("click", () => {openModal(item);});
         container.appendChild(card);
         
     });
@@ -38,8 +43,28 @@ function filterSolutions(){
     });
  renderSolutions(filtered);
 }
+function openModal(solution){
+    modalTitle.textContent = solution.title;
+    modalDescription.textContent = solution.description;
+    modalStatul.textContent = solution.status;
+    modal.classList.remove("hidden");
+}
+closeBtn.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+widow.addEventListener("click", (e) => {
+    if (e.target === modal){
+        modal.classList.add("hidden");
+    }
+});
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape"){
+        modal.classList.add("hidden");
+    }
+});
 statusSelect.addEventListener("change", filterSolutions);
 searchInput.addEventListener("input", filterSolutions);
+
 
 
 
