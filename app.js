a1f3ab933fd6c2806887bffbd6167791bc226086
VladChepesh <nourovv@gmail.com>
2026-02-25 15:56:21 +0100

    app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ function openModal(solution){
     modalOwner.textContent = solution.owner;
     modalStatus.textContent = solution.status;
     const indicator = document.getElementById("modalStatusIndicator");
-    indicator.className = "status-indicator"+ getStatusColorClass(item.status);
+    indicator.className = "status-indicator"+ getStatusColorClass(solution.status);
     modalLink.textContent = solution.link;
     modal.classList.remove("hidden");
 }
@@ -90,6 +90,7 @@ document.addEventListener("keydown", (e) => {
 });
 statusSelect.addEventListener("change", filterSolutions);
 searchInput.addEventListener("input", filterSolutions);
+
 
 
 
