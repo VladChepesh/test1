50bc093f7b7c7594de85b0d0fdeff366fc5a8ec1
VladChepesh <nourovv@gmail.com>
2026-03-16 08:56:39 +0100

    app.js

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ function renderSolutions(data){
         <h3>${item.name}</h3>
         <p>${item.descriptionShort}</p>
         <p>
-        <span class="status-indicator ${getStatusColorClass(item.status)}"></span>
         <strong>Status:</strong>${item.status}
+        <span class="status-indicator ${getStatusColorClass(item.status)}"></span>
         </p>
         <p><strong>Category:</strong>${item.category}</p>
         <p><strong>Kontakt:</strong>${item.owner}</p>
